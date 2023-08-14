d62a5cf674647bc211cdad0fc52100c0966c3df0
ErnErnKoh <shi.hao.koh@emory.edu>
2023-08-14 11:26:38 -0400

    comment on codes

---
+++ data/clean-data-bad.R
@@ -0,0 +1,24 @@
+nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
+							 "id", "nsibs", "samp", "race_eth", "sex", "region",
+							 "income", "res_1980", "res_2002", "age_bir")
+
+library(tidyverse)
+setwd("C:/Users/ErnestYogi/EPI590R/epi590r-in-class/data/raw")
+#this doesn't work due to different file path.
+nlsy <- read_csv("nlsy.csv",
+								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
+								 skip = 1, col_names = nlsy_cols)
+
+library(dplyr)
+nlsy <- nlsy |>
+	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+				 sex_cat = factor(sex, labels = c("Male", "Female")),
+				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
+				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
+				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
+
+nlsy <- na.omit(nlsy)
+
+setwd("../clean/")
+#this doesn't work due to different file path
+write_rds(nlsy, "nlsy-complete-cases.rds")
