41c61ac6d3dcf892341fdc9dabf535636fb59625
ErnErnKoh <shi.hao.koh@emory.edu>
2023-08-16 00:55:56 -0400

    Aug 16 12:55am

---
+++ R/functions.R
@@ -0,0 +1,44 @@
+x <- c(1,2,3,4,7,9)
+
+n <- length(x)
+sum(x)/n
+
+new_mean <- function(x){sum(x)/n}
+
+prop <- function(x){
+	n <- length(x)
+	mean_val <-
+}
+
+#exercise
+
+x <- 2
+x^2
+square <- function(x){
+	n <- sum(x)
+	val <- n^2
+	return(val)
+	}
+
+square(c(1,2,3,4))
+
+
+square(x)
+square(56)
+
+raise <- function(x, power = 2){
+	n <- sum(x)
+	val <- n^power
+	return(val)
+}
+
+raise <- function(x, power){
+	n <- sum(x)
+	val <- n^power
+	return(val)
+}
+
+raise(c(1,2,3,4))
+raise(4)
+raise(2, power = 4)
+
